2ac2f48c6dbf555ec96192315451a23874a2294c
Paul Sztajer <paul@seethroughstudios.com>
2018-01-23 17:17:18 +0800

    Adding basic utilities

---
+++ Editor/LocalisationConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using I2.Loc;
+
+public class LocalisationConfig {
+
+    [MenuItem("Assets/Configuration/Localisation")]
+    public static void Config()
+    {
+        GameObject src = Resources.Load("I2Languages") as GameObject;
+        Selection.activeObject = src;
+        EditorUtility.FocusProjectWindow();
+    }
+}
---
+++ ScriptLocalization.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace I2.Loc
+{
+	public static class ScriptLocalization
+	{
+		public static string Get(string Term, bool FixForRTL=true, int maxLineLengthForRTL=0, bool ignoreRTLnumbers=true, bool applyParameters=false, GameObject localParametersRoot=null, string overrideLanguage=null) { return LocalizationManager.GetTranslation(Term, FixForRTL, maxLineLengthForRTL, ignoreRTLnumbers, applyParameters, localParametersRoot, overrideLanguage); }
+
+
+	}
+}
